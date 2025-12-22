8a9a2c5fc39382636e031ce4e26f816db71b1a53
GAURAV KARKI <gaurav22013105@iimscollege.edu.np>
2025-12-22 15:16:52 +0545

    Docs: adding comments in the code

--- matdan/accounts/views.py
+++ matdan/accounts/views.py
@@ -15,17 +15,26 @@ def accounts(request):
 
 
 class CustomAuthToken(ObtainAuthToken):
+    """
+    Custom authentication token view that extends DRF's default `ObtainAuthToken` class.
+    This view provides a token upon sucessful login and also update `last_login` timestamp.
+    """
     def post(self, request, *args, **kwargs):
+        #Instantiate the serializer with the request data.
         serializer = self.serializer_class(data=request.data, context={'request':request})
 
+        #validate the credentails. if invalid , it will raise Validation Error.
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
+
+        # Retrive an existing token or create a new one for the user.
         token, created = Token.objects.get_or_create(user=user)
 
         #manually update the last login field
         user.last_login = timezone.now()
         user.save(update_fields=['last_login']) # save the last login time to the database
 
+        # Return a customresponse including the token and basic user info.
         return Response({
             'token':token.key,
             'user_id':user.pk,
@@ -37,6 +46,8 @@ class UserRegistrationView(generics.CreateAPIView):
     """
     API endpoint for creating new user instance.
     """
+    # Use the UserRegistrationSerializer to validate and create a user instance.
     serializer_class = UserRegistrationSerializer
+    #Allow any user (authenticated or not) to acces this endpoint for registration
     permission_classes =[permissions.AllowAny]
 
