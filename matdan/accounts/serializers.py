7e7882c13e6c0b1607a9e58136b2ee059509a8d9
GAURAV KARKI <gaurav22013105@iimscollege.edu.np>
2025-12-22 15:07:58 +0545

    Docs: Adding comments in the code

--- matdan/accounts/serializers.py
+++ matdan/accounts/serializers.py
@@ -3,16 +3,22 @@ from rest_framework import serializers
 from .models import User
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True, style={'input_type':'password'})
+    """
+    serializer for creating user.
+    """
+    password = serializers.CharField(write_only=True, required=True, style={'input_type':'password'}) # 'style={'input_type': 'password'}' helps DRF's browsable API render this as a password input field.
 
     # Meta class provides the metadata of the model and field to be included in the serializer
     class Meta:
         model = User
-        #list of the field from the user model(i.e. accounts/model.py) that will be included in the serialized data
+        # Tuple of the field from the user model(i.e. accounts/model.py) that will be included in the serialized data
         fields = ('username', 'password', 'email', 'wallet_address', 'national_id_hash')
 
     #method to create a new user instance when valid data is submitted to the serializer
     def create(self, validated_data):
+        """
+        This method is called to create a new user instance when valid data is submitted.
+        """
         user = User.objects.create_user(
             username = validated_data['username'],
             email = validated_data.get('email'),
@@ -20,6 +26,7 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
             wallet_address = validated_data.get('wallet_address'),
             national_id_hash = validated_data.get('national_id_hash')
         )
+        # return the newly created user instance.
         return user
     
         
